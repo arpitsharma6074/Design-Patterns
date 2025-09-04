11e2da197bc54cbe9f242128d001e8c2943eed72
arpit sharma <itssharma6074@gmail.com>
2025-09-04 17:47:58 +0530

    Update Factory.java

--- Factory.java
+++ Factory.java
@@ -1,2 +1,17 @@
+package Design-Patterns;
+
 public class Factory {
+    private Factory(){
+    }
+    public static Factory of(){
+        // Logic for Object creation
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        // Instead of constructor directly we use static method for
+        // constructor involcation
+        Factory f = Factory.of();
+    }
 }
