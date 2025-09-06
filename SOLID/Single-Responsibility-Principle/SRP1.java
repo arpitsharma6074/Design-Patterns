3291e6665ed20de5be6ee429e46e166fc4080416
arpit sharma <itssharma6074@gmail.com>
2025-09-06 11:24:45 +0530

    Implemented Single Responsibility principle

--- SOLID/Single-Responsibility-Principle/SRP1.java
+++ SOLID/Single-Responsibility-Principle/SRP1.java
@@ -1,5 +1,21 @@
 package Design-Patterns.SOLID.Single-Responsibility-Principle;
 
 public class SRP1 {
-    
+//    Single Responsibility Principle (SRP)
+//    This principle states that a class should have only one reason to change,
+//    meaning it should have only one job or responsibility.
+//    A good way to think about this is that a class should be responsible for
+//    a single part of the functionality of the software.
+}
+
+// Bad example - violates SRP
+class Employee {
+    public double calculatePay() {
+        // logic to calculate employee pay
+        return 0.0;
+    }
+    public void generateReport() {
+        // logic to generate a report for the employee
+        System.out.println("Generating employee report...");
+    }
 }
--- SOLID/Single-Responsibility-Principle/SRP2.java
+++ SOLID/Single-Responsibility-Principle/SRP2.java
@@ -1,5 +1,23 @@
 package Design-Patterns.SOLID.Single-Responsibility-Principle;
 
 public class SRP2 {
+//    The Employee class in previous implementation has two reasons to change:
+//    if the pay calculation logic changes or if the report generation logic changes.
 
+//    A better approach is to separate these responsibilities into different classes.
+}
+
+
+// Good example - follows SRP
+class Employee {
+    public double calculatePay() {
+        // logic to calculate employee pay
+        return 0.0;
+    }
+}
+class EmployeeReportGenerator {
+    public void generateReport(Employee employee) {
+        // logic to generate a report for the employee
+        System.out.println("Generating employee report...");
+    }
 }
