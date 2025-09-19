817e25665b310965b1fcfa722b93bf58d4da7109
arpit sharma <itssharma6074@gmail.com>
2025-09-19 23:31:28 +0530

    Added synchronized version of singleton object creation

--- Singleton.java
+++ Singleton.java
@@ -15,4 +15,18 @@ Public class Singleton{
             return singleton;
         }
     }
+    // The problem with above of method is, if two object access the method
+    // at the same time then there might be possible that two different object
+    // will get created.
+
+
+    // To overcome the problem we can use synchronized version of the same method
+    public static synchronized Singleton of2(){
+        if(singleton == null) {
+            singleton = new Singleton();
+            return singleton;
+        }else {
+            return singleton;
+        }
+    }
 }
