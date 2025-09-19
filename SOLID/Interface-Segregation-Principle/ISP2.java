2c74bb67e37badbc29d785c603efb9ca88d2d7c0
arpit sharma <itssharma6074@gmail.com>
2025-09-19 23:09:13 +0530

    Update LSP2.java

--- SOLID/Interface-Segregation-Principle/ISP2.java
+++ SOLID/Interface-Segregation-Principle/ISP2.java
@@ -2,34 +2,46 @@ package Design-Patterns.SOLID.Interface-Segregation-Principle;
 
 public class ISP2{
 /*
-    The Interface Segregation Principle states that clients should not be forced to depend on interfaces they do not use. It’s better to have many small, specific interfaces than one large, all-encompassing interface.
+    The Interface Segregation Principle states that clients should not be
+    forced to depend on interfaces they do not use. It’s better to have many
+    small, specific interfaces than one large, all-encompassing interface.
  */
 }
-/*
-    A better approach is to design the hierarchy in a way that all
-    subclasses can perform the same actions as the superclass.
- */
-// Good example - follows LSP
-interface Flight {
-    void fly();
+
+//   A better approach is to break down the interface into smaller,
+//   more specific ones.
+
+
+// Good example - follows ISP
+interface Printable {
+    void print();
+}
+interface Scannable {
+    void scan();
+}
+interface Faxable {
+    void fax();
 }
 
-class Bird {
-    // common bird behaviors, e.g., eat()
-}
 
-class FlyingBird extends Bird implements Flight {
+class SimplePrinter implements Printable {
     @Override
-    public void fly() {
-        // flying logic
+    public void print() {
+        System.out.println("Printing...");
+    }
+}
+class PhotoCopier implements Printable, Scannable {
+    @Override
+    public void print() {
+        System.out.println("Printing...");
+    }
+    @Override
+    public void scan() {
+        System.out.println("Scanning...");
     }
 }
 
-class Ostrich extends Bird {
-    // an ostrich can't fly, so it doesn't implement the Flight interface.
-}
-
 /*
-Now, a client can rely on the Flight interface and use any object that
-implements it, like FlyingBird, without fear of unexpected behavior.
+    Now, a class only needs to implement the interfaces for the functionality
+    it actually supports.
  */
