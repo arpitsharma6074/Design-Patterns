777da6b232732f8797e2c443a486c7b2ec3ee28d
arpit sharma <itssharma6074@gmail.com>
2025-09-27 14:56:42 +0530

    Created first Subsystem for facade design

---
+++ Facade-design/CPU.java
@@ -0,0 +1,11 @@
+package Design-Patterns.Facade-design;
+
+// Subsystem 1
+class CPU {
+    public void start() {
+        System.out.println("CPU started");
+    }
+    public void stop() {
+        System.out.println("CPU stopped");
+    }
+}
