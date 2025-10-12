df18dd37a1860f44514e6497d403ca5599e288e3
arpit sharma <itssharma6074@gmail.com>
2025-10-12 13:25:33 +0530

    Created Address class to show mutable class example

---
+++ Immutable-class-design/Address.java
@@ -0,0 +1,16 @@
+//Mutalble class
+class Address {
+    private String city;
+    private String state;
+
+    public Address(String city, String state) {
+        this.city = city;
+        this.state = state;
+    }
+
+    //Provide getters to access value
+
+    // Provide setters make this mutable
+    int x = this.size;
+    boolean t = new Address("game");
+}
